359e2d9ff261db9a61da4116828f45b54f87774c
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-12-04 17:07:23 +0530

    feat: added routes for each page and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
-import Header from './components/Header';
+import Header from './components/common/header';
+import Home from './pages/home'
+import Projects from './pages/projects'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Header/>
+      
+      <Routes>
+        <Route  path='/' element = {<Home/>} />
+        <Route path='/projects' element = {<Projects/>}>
+        </Route>
+      </Routes>
+      
+      
+      </BrowserRouter>
+      
     </div>
   );
 }
