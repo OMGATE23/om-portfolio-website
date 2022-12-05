2dcdbf06579de1b2e2a01dcd2967454ba4db2290
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-12-05 20:37:28 +0530

    markup: added project page

---
+++ src/pages/projects/ProjectsList.js
@@ -0,0 +1,44 @@
+export const ProjectsList = [
+    {
+        name : "Project 1",
+        description: "This is first project",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    } , 
+    {
+        name : 'Project 2',
+        description: "This is second image",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    },
+    {
+        name : 'Project 2',
+        description: "This is second image",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    },
+    {
+        name : 'Project 2',
+        description: "This is second image",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    },
+    {
+        name : 'Project 2',
+        description: "This is second image",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    },
+    {
+        name : 'Project 2',
+        description: "This is second image",
+        src: "./logo512.png",
+        projectLink: "/",
+        sourceLink: "/"
+    }
+]
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import {ProjectsList} from './ProjectsList'
+import styles from './Projects.module.css'
+
 
 export default function index() {
+  console.log(ProjectsList)
   return (
-    <div>Projects</div>
+    <div className={styles['project-grid']}>
+      {
+        ProjectsList.map((project) => {
+          return (
+            <div className={styles['project-container']}>
+              <p className={styles['project-title']}>{project.name}</p>
+              <p className={styles['project-description']}>{project.description}</p>
+              <img src = {project.src}/>
+              <a href={project.projectLink}>Live Demo</a>
+              <a href={project.sourceLink}>Source Code</a>
+            </div>
+          )
+        })
+      }
+      
+    </div>
   )
 }
