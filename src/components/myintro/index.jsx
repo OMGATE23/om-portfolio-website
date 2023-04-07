3964e4fafa46c73f704eec88088425501b61ce36
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-07 10:24:47 +0530

    component : added hero component

--- src/components/myintro/index.jsx
+++ src/components/myintro/index.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import styles from './MyIntro.module.css'
-
+import React from "react";
 export default function index() {
   return (
-    <div className={styles['main-container']}>
-        <div className={styles['main-description']}>
-            <h1 className={styles['description-title']}>Hi! I am <div>Om Gate</div></h1>
-            <p>I am an Aspiring Full-stacks developer currently in second year of Engineering</p>
-        </div>
-        <div className={styles['image-container']}>
-            <div className={styles['main-image-backdrop']}></div>
-            <img className={styles['main-image']} src='./images/photo-no-bg.png'/>
-        </div>
-        
+    <div className="flex items-center justify-center h-[90vh]">
+      <h1 className="text-9xl font-semibold text-center">
+        "Building beautiful websites, <span className="text-textColor">one line of code at a time."</span>
+      </h1>
     </div>
-  )
+  );
 }
