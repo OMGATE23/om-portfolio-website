4d797d90e17009bc29af1f20cbd96263d5506b3e
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-07 10:24:29 +0530

    component : added footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-[20vh] flex flex-col justify-center items-center bg-primaryColor">
+      <ul className="h-[10vh] w-[20rem] mt-4 mx-auto flex items-center justify-around"> 
+          
+        <li>
+          <a href="https://github.com/OMGATE23">
+            <i class="fa-brands fa-github text-3xl text-white"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/om-gate-68a5a3201/">
+            <i class="fa-brands fa-linkedin text-3xl text-white"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/om_gate">
+            <i class="fa-brands fa-twitter text-3xl text-white"></i>
+          </a>
+        </li>
+      </ul>
+      <p className="my-4 text-white">©This Website belongs to Om Gate</p>
+    </div>
+  );
+};
+
+export default Footer;
