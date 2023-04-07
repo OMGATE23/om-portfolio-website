8d3043932f4faa4eaf20299e2e10e2049eed4516
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-07 10:26:26 +0530

    data: added projects list

--- src/pages/projects/ProjectsList.js
+++ src/pages/projects/ProjectsList.js
@@ -1,44 +1,27 @@
+import homeBudget from '../../assets/HomeBudget.png'
+import nebulaFontify from '../../assets/NebulaFontify.png'
+import financeTracker from '../../assets/FinanceTracker.png'
+
 export const ProjectsList = [
     {
-        name : "Project 1",
-        description: "This is first project",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
+        name : "HomeBudget",
+        description: "This app will help you create budgets and help understand your finances",
+        src: homeBudget,
+        projectLink: "https://budget-aerbl1jiw-omgate23.vercel.app/",
+        sourceLink: "https://github.com/OMGATE23/budget-app/"
     } , 
     {
-        name : 'Project 2',
-        description: "This is second image",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
+        name : 'NebulaFontify',
+        description: "Create stunning typographic designs for any website",
+        src: nebulaFontify,
+        projectLink: "https://nebulafontify.vercel.app/",
+        sourceLink: "https://github.com/OMGATE23/nebula-fontify"
     },
     {
-        name : 'Project 2',
-        description: "This is second image",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
-    },
-    {
-        name : 'Project 2',
-        description: "This is second image",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
-    },
-    {
-        name : 'Project 2',
-        description: "This is second image",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
-    },
-    {
-        name : 'Project 2',
-        description: "This is second image",
-        src: "./logo512.png",
-        projectLink: "/",
-        sourceLink: "/"
+        name : 'My Finance Tracker App',
+        description: "This app helps you to keep track of your finances",
+        src: financeTracker,
+        projectLink: "http://my-finance-tracker.vercel.app/",
+        sourceLink: "https://github.com/OMGATE23/my-finance-tracker"
     }
 ]
