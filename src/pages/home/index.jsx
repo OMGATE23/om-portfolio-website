47c709fdf919bd286118fe2a916e2eea61702f1c
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-07 10:25:47 +0530

    page: added components to home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import MyIntro from '../../components/myintro'
-
+import Aboutme from '../../components/aboutme'
+import Content from '../../components/content'
 export default function index() {
   return (
-    <>
+    <div className='bg-white'>
         <MyIntro/>
-    </>
+        <Aboutme/>
+        <Content/>
+    </div>
   )
 }
