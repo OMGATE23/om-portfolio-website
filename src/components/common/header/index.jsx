34049229848fa19693a1834c7fb1842c19d7241c
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-04-07 10:23:55 +0530

    component: added header

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
-import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
 export default function Header() {
   return (
-    <header>
-        <h2 className={styles['header-name']}>Om Gate</h2>
-
-        <ul className={styles['header-list']} >
-            <li>
+    <nav className='sticky top-0 left-0 backdrop-blur-xl text-textColor flex rounded-b-xl shadow-md items-center h-[10vh]'>
+        <h2 className='justify-self-start ml-8 text-4xl font-semibold '>Om Gate</h2>
+        <div className='h-full text-tex justify-self-center  mx-auto'>
+        <ul className=' h-full flex items-center gap-24 text-xl '>
+            <li className='transition-transform duration-300 hover:border-b-2 hover:translate-y-[-5px] border-black font-bold hover:cursor-pointer'>
                 <Link to='/'>
                     Home
                 </Link>
             </li>
 
-            <li>
+            <li className='transition-transform duration-300 hover:border-b-2 hover:translate-y-[-5px] border-black font-bold hover:cursor-pointer'>
                 <Link to='/projects'>
                     Projects
                 </Link>
             </li>
 
-            <li>
+            <li className='transition-transform duration-300 hover:border-b-2 hover:translate-y-[-5px] border-black font-bold hover:cursor-pointer'>
                 <Link to = '/blogs'>
                     Blogs
                 </Link>
             </li>
         </ul>
-    </header>
+        
+        </div>
+        <h2 className='justify-self-start mr-8 text-4xl font-semibold outline outline-2 text-transparent'>Om Gate</h2>
+    </nav>
   )
 }
