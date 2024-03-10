5de2f0bdd88cdffc014192500f4ee0445c6d3c41
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-10 18:42:43 +0530

    style : removed old styles
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ["Poppins"],
-      },
-      colors: {
-        textColor: "#252B42",
-        secondaryTextColor: "#737373",
-        primaryColor : 'rgb(37, 43, 66)',
-        primaryColorFaded75 : 'rgba(37, 43, 66 , 0.75)',
-        primaryColorFaded60 : 'rgba(37, 43, 66 , 0.6)',
-        primaryColorFaded50 : 'rgba(37, 43, 66 , 0.5)',
-        backgroundColor: "#EDA751",
-        backgroundColorFaded : 'rgba( 237 , 167 , 81 , 0.75 )',
-        LightGray02 : 'rgba( 0, 0, 0,0.02)',
-        LightGray05 : 'rgba( 0, 0, 0,0.05)',
-        LightGray1 : 'rgba( 0, 0, 0,0.1)'
+        inter: ["Inter", "san-serif"],
       },
     },
   },
